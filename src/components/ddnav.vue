8dae2ca3ceb4e29416a4ba6d95feb7102157f36c
surze <surzepp@gmail.com>
2021-09-26 01:02:11 +0700

    add components

---
+++ src/components/ddnav.vue
@@ -0,0 +1,67 @@
+<template>
+    <button class="dropdown-toggle hover:text-green-500" type="button" id="dropdownMouse" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover:text-green-500 text-md font-bold">MOUSE</span></button>
+              <ul class="dropdown-menu dropdown-menu-dark p-2 m-2 -mt-3" aria-labelledby="dropdownMouse">
+                <li><a class="text-white dropdown-item font-semibold" href="#">Steelseries</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Logitech</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Corsair</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Razer</a></li>
+              </ul>
+    <button class="dropdown-toggle hover:text-green-500" type="button" id="dropdownMonitor" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover:text-green-500 text-md font-bold">Monitor</span></button>
+              <ul class="dropdown-menu dropdown-menu-dark p-2 m-2 -mt-3" aria-labelledby="dropdownMonitor">
+                <li><a class="text-white dropdown-item font-semibold" href="#">Steelseries</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Logitech</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Corsair</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Razer</a></li>
+              </ul>
+    <button class="dropdown-toggle hover:text-green-500" type="button" id="dropdownMousepad" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover:text-green-500 text-md font-bold">Mousepad</span></button>
+              <ul class="dropdown-menu dropdown-menu-dark p-2 m-2 -mt-3" aria-labelledby="dropdownMousepad">
+                <li><a class="text-white dropdown-item font-semibold" href="#">Steelseries</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Logitech</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Corsair</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Razer</a></li>
+              </ul>
+    <button class="dropdown-toggle hover:text-green-500" type="button" id="dropdownKeyboard" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover:text-green-500 text-md font-bold">Keyboard</span></button>
+              <ul class="dropdown-menu dropdown-menu-dark p-2 m-2 -mt-3" aria-labelledby="dropdownKeyboard">
+                <li><a class="text-white dropdown-item font-semibold" href="#">Steelseries</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Logitech</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Corsair</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Razer</a></li>
+              </ul>
+    <button class="dropdown-toggle hover:text-green-500" type="button" id="dropdownHeadphone" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover:text-green-500 text-md font-bold">Headphone</span></button>
+              <ul class="dropdown-menu dropdown-menu-dark p-2 m-2 -mt-3" aria-labelledby="dropdownHeadphone">
+                <li><a class="text-white dropdown-item font-semibold" href="#">Steelseries</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Logitech</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Corsair</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Razer</a></li>
+              </ul>
+    <button class="dropdown-toggle hover:text-green-500" type="button" id="dropdownController" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover:text-green-500 text-md font-bold">Controller</span></button>
+              <ul class="dropdown-menu dropdown-menu-dark p-2 m-2 -mt-3" aria-labelledby="dropdownController">
+                <li><a class="text-white dropdown-item font-semibold" href="#">Steelseries</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Logitech</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Corsair</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="text-white dropdown-item font-semibold" href="#">Razer</a></li>
+              </ul>
+</template>
+<script>
+    export default{
+        name: 'ddnav'
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 import Navbar from './components/Navbar.vue'
+import ddnav from './components/ddnav.vue'
+import footer from './components/footer.vue'
 const app = createApp(App)
 app.component('nav-bar',Navbar)
+app.component('dd-nav',ddnav)
+app.component('footer-com',footer)
 app.use(router).mount('#app')
