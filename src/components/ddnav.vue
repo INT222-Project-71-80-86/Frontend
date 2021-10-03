96dde193ad89a91a28d0d5ed53b90f5845338f6e
surze <surzepp@gmail.com>
2021-10-04 01:43:49 +0700

    v-for & filter function

--- src/components/ddnav.vue
+++ src/components/ddnav.vue
@@ -1,67 +1,32 @@
 <template>
-    <button class="dropdown-toggle hover:text-green-500" type="button" id="dropdownMouse" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover:text-green-500 text-md font-bold">MOUSE</span></button>
+<span v-for="category in categories" :key="category.catid">
+    <button class="dropdown-toggle hover:text-green-500" type="button" id="dropdownMouse" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover:text-green-500 text-md font-bold">{{ category.name }}</span></button>
               <ul class="dropdown-menu dropdown-menu-dark p-2 m-2 -mt-3" aria-labelledby="dropdownMouse">
-                <li><a class="text-white dropdown-item font-semibold" href="#">Steelseries</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Logitech</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Corsair</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Razer</a></li>
+                <span v-for="brand in brands" :key="brand.bid">
+                    <li>
+                      <a class="text-white dropdown-item font-semibold" 
+                      :href="$router.resolve({ name: 'showproducts', params: { type: category.catid, value: brand.bid } }).href">
+                      {{ brand.name }}
+                      </a>
+                    </li>
+                  <li><hr class="dropdown-divider"></li>
+                </span>
               </ul>
-    <button class="dropdown-toggle hover:text-green-500" type="button" id="dropdownMonitor" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover:text-green-500 text-md font-bold">Monitor</span></button>
-              <ul class="dropdown-menu dropdown-menu-dark p-2 m-2 -mt-3" aria-labelledby="dropdownMonitor">
-                <li><a class="text-white dropdown-item font-semibold" href="#">Steelseries</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Logitech</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Corsair</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Razer</a></li>
-              </ul>
-    <button class="dropdown-toggle hover:text-green-500" type="button" id="dropdownMousepad" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover:text-green-500 text-md font-bold">Mousepad</span></button>
-              <ul class="dropdown-menu dropdown-menu-dark p-2 m-2 -mt-3" aria-labelledby="dropdownMousepad">
-                <li><a class="text-white dropdown-item font-semibold" href="#">Steelseries</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Logitech</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Corsair</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Razer</a></li>
-              </ul>
-    <button class="dropdown-toggle hover:text-green-500" type="button" id="dropdownKeyboard" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover:text-green-500 text-md font-bold">Keyboard</span></button>
-              <ul class="dropdown-menu dropdown-menu-dark p-2 m-2 -mt-3" aria-labelledby="dropdownKeyboard">
-                <li><a class="text-white dropdown-item font-semibold" href="#">Steelseries</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Logitech</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Corsair</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Razer</a></li>
-              </ul>
-    <button class="dropdown-toggle hover:text-green-500" type="button" id="dropdownHeadphone" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover:text-green-500 text-md font-bold">Headphone</span></button>
-              <ul class="dropdown-menu dropdown-menu-dark p-2 m-2 -mt-3" aria-labelledby="dropdownHeadphone">
-                <li><a class="text-white dropdown-item font-semibold" href="#">Steelseries</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Logitech</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Corsair</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Razer</a></li>
-              </ul>
-    <button class="dropdown-toggle hover:text-green-500" type="button" id="dropdownController" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover:text-green-500 text-md font-bold">Controller</span></button>
-              <ul class="dropdown-menu dropdown-menu-dark p-2 m-2 -mt-3" aria-labelledby="dropdownController">
-                <li><a class="text-white dropdown-item font-semibold" href="#">Steelseries</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Logitech</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Corsair</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="text-white dropdown-item font-semibold" href="#">Razer</a></li>
-              </ul>
+</span>
 </template>
 <script>
+import { computed } from '@vue/reactivity';
+import { useStore } from 'vuex';
     export default{
-        name: 'ddnav'
+        name: 'ddnav',
+        setup(){
+          const store = useStore();
+          store.dispatch('fetchAllBrands')
+          store.dispatch('fetchAllCategories')
+          return {
+            brands: computed(() => store.state.brands),
+            categories: computed(() => store.state.categories)
+    }
+        }
     }
 </script>
