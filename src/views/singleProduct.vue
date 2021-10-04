7714fbdcb36e1562cbf1a4c95842469c13281f37
surze <surzepp@gmail.com>
2021-10-04 21:09:19 +0700

    show review

--- src/views/singleProduct.vue
+++ src/views/singleProduct.vue
@@ -1,6 +1,6 @@
 <template>
   <nav-bar class="z-50" />
-<body>
+<body class="bg-gray-900">
   
 <div class="container-fluid mt-2 mb-3">
     <div class="row no-gutters">
@@ -40,41 +40,42 @@
             </div>
         </div>
         <div class="col-md-7">
-            <div class="card mt-10 ">
-            <div class="tracking-wider mb-2">
+            <div class="card mt-3 ">
+            <div class="tracking-wider space-y-1">
                 <div class="d-flex flex-row align-items-center">
-                    <div class="p-ratings">** Wating for Rating ** </div>
+                    <div >** Wating for Rating **</div>
                 </div>
-                <div class="border-b"> <span class="font-weight-bold"> {{ product.name }} </span> <span> | </span> <span> {{ product.category.name }} </span><span> | </span> <span> {{ product.brand.name }} </span>
-                    <h4 class="font-weight-bold">{{ pricenumber(product.price) }} THB.-</h4>
+                <div class="space-y-2 text-center "> <span class="font-bold text-xl italic">— {{ product.name }} —</span> <span> | </span> <span class="text-gray-600"> {{ product.category.name }} </span><span> | </span> <span class="text-gray-600"> {{ product.brand.name }} </span>
+                    <h1 class="font-semibold text-right">{{ pricenumber(product.price) }} THB.-</h1>
                 </div>
+                <hr class="mt-3 mb-2" />
                 </div>
                 
-                <div class="product-description">
-                    <div class="flex space-x-2 space-y-1"><span class="font-weight-bold mt-2">Color — </span> 
+                <div class="product-description tracking-wider">
+                    <div class="flex space-x-2 space-y-1"><span class="font-semibold mt-2">Color — </span> 
                         <div class="flex items-center" v-for="c in product.productcolor" :key="c.cid">
-                                <div class="color-circle rounded-full w-7 h-7" :style="{ backgroundColor: c.color.code }"></div>
+                                <div class="color-circle rounded-full w-7 h-7 border-2 border-transparent hover:border-gray-900 duration-300" :style="{ backgroundColor: c.color.code }"></div>
                             </div>
                                 </div>
-                    <div class="my-color">  </div>
-                    <div class=" flex-row align-items-center"> <i class="fa fa-calendar-check-o"></i> <span>Release Date: {{product.releaseDate}} </span><span> | </span> <span>Warranty: {{product.warranty}} </span></div>
-                    <div class="mt-2"> <span class="font-weight-bold">— Description —</span>
-                        <p>— {{ product.description }}</p>
+                    <div class="flex-row align-items-center mt-2"> <span class="font-semibold">Release Date:</span> <span class="text-sm tracking-widest">{{product.releaseDate}}</span> <span> | </span> <span class="font-semibold">Warranty:</span> <span class="tracking-widest text-sm">{{product.warranty}} year</span></div>
+                    <div class="mt-2"> <span class="font-semibold">— Description —</span>
+                        <p class="text-center">{{ product.description }}</p>
                        
                     </div>
                     <div class="mt-2"> <span class="font-weight-bold">STORE</span> </div>
-                    <div class="d-flex flex-row align-items-center"> <router-link :to="{ name: 'showproducts', params: { type: 'all', value: '1' } }"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-arrow-left-square-fill" viewBox="0 0 16 16">
+                    <div class="d-flex flex-row align-items-center"> <router-link :to="{ name: 'showproducts', params: { type: 'all', value: '1' } }"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-arrow-left-square-fill duration-300" viewBox="0 0 16 16">
   <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z"/>
 </svg></router-link>
                         <div class="d-flex flex-column ml-1 comment-profile">
-                            <div class="comment-ratings"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> </div> <span class="username">NPN | SHOP</span> <small class="followers">Shopping more...</small>
+                            <div class="shopping-more"> </div> <span class="username">NPN | SHOP</span> <small class="followers">Shopping more...</small>
                         </div>
                     </div>
-                    <div class="buttons "> <button class="btn btn-outline-warning btn-long cart">Add to Cart</button>  </div>
+                    <div> <button class="border-2 border-black pl-5 pr-5 pt-2 pb-2 cart mt-3 hover:bg-green-400 hover:text-white duration-500">Add to Cart</button>  </div>
                 </div>
             </div>
-            <div class="card mt-2"> <span>Similar items:</span>
-                <div class="similar-products mt-2 d-flex flex-row">
+            <!--Similar item-->
+            <div class="card mt-2 border text-center"> <span>Similar items:</span>
+                <div class="similar-products mt-2 d-flex flex-row justify-center space-x-3">
                     <div class="card border p-1" style="width: 9rem;margin-right: 3px;"> <img src="https://i.imgur.com/KZpuufK.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h6 class="card-title">$1,999</h6>
@@ -102,12 +103,19 @@
                     </div>
                 </div>
             </div>
-            <div class="card mt-2"> <span>Review:</span>
-                text field
+            <!-- end similar items -->
+            <div class="card mt-2 mb-3">
+                <star-rating :rating="avgRating" :increment="0.01" :read-only="true" :star-size="24" :show-rating="false" :glow="1"></star-rating>
+            
+            <span class="mb-2 text-xl font-bold uppercase tracking-wide">Write Your Review:</span>
+            
+            <div>
+                <base-review :pid="singleProd" @calculate="calRating"></base-review>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
 <!--<div><p> name - {{product.name}} </p></div>
 <div><p> desc - {{product.description}} </p></div>
 <div><p> warranty - {{product.warranty}} </p></div>
@@ -124,18 +132,20 @@
 </body>
 </template>
 <script>
-
 import axios from "axios";
+import StarRating from 'vue-star-rating';
+import BaseReview from '../components/BaseReview.vue';
 export default {
-    
-    name: 'singleProduct',
-    props: ["singleProd"],
-    data() {
-        return {
-            backend_url: process.env.VUE_APP_BACKEND_URL,
-            product: {},
-        };
-    },
+  components: { BaseReview, StarRating },
+  name: "singleProduct",
+  props: ["singleProd"],
+  data() {
+    return {
+      backend_url: process.env.VUE_APP_BACKEND_URL,
+      product: {},
+      avgRating: 0
+    };
+  },
     methods: {
         async fetchProduct(pid) {
       const res = await axios.get(`${this.backend_url}/product/${pid}`);
@@ -146,6 +156,14 @@ export default {
     },
      pricenumber(price){
      return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }, 
+    calRating(r){
+        let sum = 0.0
+        r.forEach(review => {
+            sum += review.rating
+        });
+        this.avgRating = sum/(r.length)
+
     }
   },
   
@@ -163,11 +181,10 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap");
 
 body {
-    background-color: #eee;
     font-family: "Poppins", sans-serif
 }
 .card {
-    background-color: #fff;
+    background-color: white;
     padding: 14px;
     border: none
 }
@@ -176,32 +193,12 @@ body {
     width: 100%
 }
 
-ul {
-    list-style: none outside none;
-    padding-left: 0;
-    margin-bottom: 0
-}
-
-li {
-    display: block;
-    float: left;
-    margin-right: 6px;
-    cursor: pointer
-}
-
 img {
     display: block;
     height: auto;
     width: 100%
 }
 
-.stars i {
-    color: #f6d151
-}
-
-.stars span {
-    font-size: 13px
-}
 
 hr {
     color: #d4d4d4
@@ -236,73 +233,6 @@ hr {
     font-size: 12px
 }
 
-.p-ratings i {
-    color: #f6d151;
-    font-size: 12px
-}
-
-.btn-long {
-    padding-left: 35px;
-    padding-right: 35px
-}
-
-.buttons {
-    margin-top: 15px
-}
-
-.buttons .btn {
-    height: 46px
-}
-
-.buttons .cart {
-    border-color: #ff7676;
-    color: #ff7676
-}
-
-.buttons .cart:hover {
-    background-color: #e86464 !important;
-    color: #fff
-}
-
-.buttons .buy {
-    color: #fff;
-    background-color: #ff7676;
-    border-color: #ff7676
-}
-
-.buttons .buy:focus,
-.buy:active {
-    color: #fff;
-    background-color: #ff7676;
-    border-color: #ff7676;
-    box-shadow: none
-}
-
-.buttons .buy:hover {
-    color: #fff;
-    background-color: #e86464;
-    border-color: #e86464
-}
-
-.buttons .wishlist {
-    background-color: #fff;
-    border-color: #ff7676
-}
-
-.buttons .wishlist:hover {
-    background-color: #e86464;
-    border-color: #e86464;
-    color: #fff
-}
-
-.buttons .wishlist:hover i {
-    color: #fff
-}
-
-.buttons .wishlist i {
-    color: #ff7676
-}
-
 .comment-ratings i {
     color: #f6d151
 }
@@ -314,81 +244,6 @@ hr {
 
 .store-image {
     width: 42px
-}
-
-.dot {
-    height: 10px;
-    width: 10px;
-    background-color: #bbb;
-    border-radius: 50%;
-    display: inline-block;
-    margin-right: 5px
-}
-
-.bullet-text {
-    font-size: 12px
-}
-
-.my-color {
-    margin-top: 10px;
-    margin-bottom: 10px
-}
-
-label.radio {
-    cursor: pointer
-}
-
-label.radio input {
-    position: absolute;
-    top: 0;
-    left: 0;
-    visibility: hidden;
-    pointer-events: none
-}
-
-label.radio span {
-    border: 2px solid #8f37aa;
-    display: inline-block;
-    color: #8f37aa;
-    border-radius: 50%;
-    width: 25px;
-    height: 25px;
-    text-transform: uppercase;
-    transition: 0.5s all
-}
-
-label.radio .red {
-    background-color: red;
-    border-color: red
-}
-
-label.radio .blue {
-    background-color: blue;
-    border-color: blue
-}
-
-label.radio .green {
-    background-color: green;
-    border-color: green
-}
-
-label.radio .orange {
-    background-color: orange;
-    border-color: orange
-}
-
-label.radio input:checked+span {
-    color: #fff;
-    position: relative
-}
-
-label.radio input:checked+span::before {
-    opacity: 1;
-    content: '\2713';
-    position: absolute;
-    font-size: 13px;
-    font-weight: bold;
-    left: 4px
 }
 
 .card-body {
