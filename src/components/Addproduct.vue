8bf27c0cdc5dc232b066c79ddf1a529ff70d5339
surze <surzepp@gmail.com>
2021-10-05 01:22:48 +0700

    add option of warranty from 2 to 3

--- src/components/Addproduct.vue
+++ src/components/Addproduct.vue
@@ -156,6 +156,7 @@
                             <option value="0">None</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
+                            <option value="3">3</option>
                         </select>
                         <p
                             v-if="invalidWarrantyInput"
