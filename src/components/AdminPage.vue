a9c8227d072f635b56b4b497634925d30a2ef6db
Paksawat Rakthai <paksawat.rakthai@mail.kmutt.ac.th>
2021-11-18 17:11:01 +0700

    add role manage component

---
+++ src/components/AdminPage.vue
@@ -0,0 +1,77 @@
+<template>
+  <nav-bar></nav-bar>
+    <ul class=" list-reset flex border-b justify-center items-center">      
+        <li class="mr-1">
+           
+            <button 
+                @click="this.case=1"
+                class="bg-white inline-block py-2 px-4 text-blue hover:text-blue-darker font-semibold"
+                
+            >Add Brand</button>
+        </li>
+         <li class="mr-1">
+            <button
+            @click="this.case=2"
+                class="bg-white inline-block py-2 px-4 text-blue hover:text-blue-darker font-semibold"
+                
+            >Add Color</button>
+        </li>
+        <li class="mr-1">
+            <button
+            @click="this.case=3"
+                class="bg-white inline-block py-2 px-4 text-blue hover:text-blue-darker font-semibold"
+                
+            >Role Manage</button>
+        </li>
+        <li class="mr-1">
+            <button
+            @click="this.case=4"
+                class="bg-white inline-block py-2 px-4 text-blue hover:text-blue-darker font-semibold"
+                
+            >Coupon Manage</button>
+        </li>
+    </ul>
+
+  
+  <component :is="selectedComponent()"></component>
+  
+
+</template>
+<script>
+
+import addbrand from "@/components/Addbrand"
+import addcolor from "@/components/Addcolor"
+import rolemanage from "@/components/RoleManage"
+
+export default {
+    name: 'AdminPage',
+    components: {
+       addbrand,
+       addcolor,
+       rolemanage
+    },
+
+    data() {
+        return {
+            case: 0,
+            stage: ''
+
+        }
+    },
+    methods: {
+
+        selectedComponent() {
+            console.log('test')
+            console.log(this.case)
+            if (this.case == 1) { this.stage = 'addbrand' }
+            else if (this.case == 2) { this.stage = 'addcolor' }
+            else if (this.case ==3){ this.stage = 'rolemanage'}
+            return this.stage
+        }
+
+    },
+
+}
+
+
+</script>
