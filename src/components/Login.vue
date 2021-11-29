e3f4d662b0b023805457e968289d2c60188f2129
surze <surzepp@gmail.com>
2021-11-29 19:38:08 +0700

    validate duplicate

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,30 +10,32 @@
   <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
 </svg>
             </div>
-            <h2 class="text-3xl text-center text-gray-700 mb-4 mt-4">NPN MEMBER</h2>
-            <div class="px-12 pb-10">
-                <div class="w-full mb-2">
+            <h2 class="text-3xl text-center text-gray-700 mt-4">NPN MEMBER</h2>
+            <h2 class="text-xl text-center text-red-500" v-if="loginfail">Invalid username or password</h2>
+            <div class="px-12 pb-10 space-y-3">
+                <div class="w-full ">
                     <div class="flex items-center">
                         <i class='ml-3 fill-current text-gray-400 text-xs z-10 fas fa-user'></i>
                         <input type='text' placeholder="Username" name="username" v-model="username"
                             class="-mx-6 w-full border rounded px-3 py-2 text-gray-700 focus:outline-none" />
+                            
                     </div>
                 </div>
-                <div class="w-full mb-2">
+                <span v-if="invalidusername" class="text-red-500 text-sm">Please Enter Your Username</span>
+                <div class="w-full ">
                     <div class="flex items-center">
                         <i class='ml-3 fill-current text-gray-400 text-xs z-10 fas fa-lock'></i>
                         <input type='password' placeholder="Password" name="password" v-model="password"
                             class="-mx-6 w-full border rounded px-3 py-2 text-gray-700 focus:outline-none" />
                     </div>
                 </div>
-                <a href="#" class="text-xs text-gray-500 float-left mb-4 -ml-2">Create Account?</a>
-                <a href="#" class="text-xs text-gray-500 float-right mb-4">Forgot Password?</a> 
+                 <span v-if="invalidpassword" class="text-red-500 text-sm">Please Enter Your Password</span>
+                <router-link to="/Register"><a href="#" class="text-xs text-gray-500 float-right mb-4 -ml-2">Create Account?</a></router-link>
                 <button type="submit"
                     class="w-full py-2 rounded-full bg-green-600 text-gray-100  focus:outline-none">Login</button>
             </div>
         </form>
     </div>
-
 </template>
 <script>
 import axios from 'axios'
@@ -44,20 +46,25 @@ data() {
         return {
             backend_url: process.env.VUE_APP_BACKEND_URL,
             username: '',
-            password: ''
+            password: '',
+            invalidusername: false,
+            invalidpassword: false,
+            loginfail: false
         }
     },
     methods: {
         async submitForm() {
+             let self = this
+             this.loginfail = false
+             this.invalidusername = this.username == null || this.username.trim() === '' ? true : false
+             this.invalidpassword = this.password == null || this.password.trim() === '' ? true : false
+             if(this.invalidusername || this.invalidpassword){
+                 return
+             }
             const formData = this.makeFormData()
             const res = await axios.post(`${this.backend_url}/login`, formData).catch(function (error) {
-            if (error.response) {
-                console.log(error)
-                console.log(error.response);
-            } else if (error.request) {
-                console.log(error.request);
-            } else {
-                console.log('Error', error.message);
+            if(error){
+                self.loginfail = true
             }
             })
             const data = res.data   
@@ -66,7 +73,6 @@ data() {
             localStorage.setItem("refresh_token",data.refresh_token) 
             const user = jwt_decode(data.access_token)
             this.$store.dispatch('fetchUser', {user, access_token})
-            
             this.$router.push( { path: "/"} )
             
         },
@@ -75,6 +81,10 @@ data() {
             formData.append("username", this.username)
             formData.append("password", this.password)
             return formData
+        },
+        validate() {
+            this.invalidusername = this.username == null || this.username.trim() === '' ? true : false
+            this.submitForm()
         }
     }
 }
