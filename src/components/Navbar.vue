52a898b7811497b56969fc0cd5322284f8b36a5c
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-12-01 11:27:39 +0700

    Add condition to animate-ping to show when there are item in cart

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -36,7 +36,7 @@
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
-              <span class="flex absolute -mt-5 ml-4">
+              <span class="flex absolute -mt-5 ml-4" v-if="cart.length > 0">
                 <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
                 <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
               </span>
@@ -109,7 +109,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
           </svg>
           </router-link>
-          <span class="flex absolute -mt-5 ml-4">
+          <span class="flex absolute -mt-5 ml-4" v-if="cart.length > 0">
             <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
             <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
             </span>
@@ -242,7 +242,8 @@ export default {
       user: computed(() => store.state.user),
       role: computed(() => store.state.role),
       brands: computed(() => store.state.brands),
-      categories: computed(() => store.state.categories)
+      categories: computed(() => store.state.categories),
+      cart: computed(() => store.state.cart)
     }
   },
   computed: {
